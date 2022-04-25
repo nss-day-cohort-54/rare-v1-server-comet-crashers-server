52d1c616be58d60ba0659d5eadcdc94e337991d8
Erich Hils <mrerichhils@gmail.com>
2022-04-25 15:49:14 -0500

    feat: commit to add category model from jacob

--- views/posts.py
+++ views/posts.py
@@ -1,51 +1,43 @@
-# the HTTP methods for posts will go here
-import sqlite3
-import json
-from datetime import datetime
+# # the HTTP methods for posts will go here
+# import sqlite3
+# import json
+# from datetime import datetime
 
-def get_all_entries():
-    with sqlite3.connect("./rare.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
+# from models.post import Post
+# from models.user import User
+
+# def get_all_posts():
+#     with sqlite3.connect("./rare.sqlite3") as conn:
+#         conn.row_factory = sqlite3.Row
         
-        db_cursor = conn.cursor()
-        db_cursor.execute("""
-        SELECT
-            e.id,
-            e.concept,
-            e.entry,
-            e.mood_id,
-            e.date,
-            t.id tag_id,
-            t.tag,
-            m.label mood_label
-        FROM Entry e  
-        JOIN Mood as m
-            On m.id = e.mood_id 
-        JOIN entrytag as et
-            On et.entry_id = e.id   
-        JOIN Tag as t
-            On t.id = et.tag_id
-        ORDER BY e.id   
-        """)
-        entries = []
+#         db_cursor = conn.cursor()
+#         db_cursor.execute("""
+#         SELECT
+#             p.id,
+#             p.user_id,
+#             p.category_id,
+#             p.title,
+#             p.date,
+#             u.username username,
+#             c.label category_label
+#         FROM Posts p  
+#         JOIN Users u
+#             On u.id = p.users_id
+#         JOIN Categories c
+#             On c.id = p.category_id  
+#         ORDER BY p.date   
+#         """)
+#         posts = []
         
-        dataset = db_cursor.fetchall()
-        entry = Entry(0,'','',0,'')
-        for row in dataset:
-            # not reset the entry if the entry exists, but still add tag onto entry's array
-            if row['id'] != entry.id:
-                # explicity set tags to a new list in model
-                entry = Entry(row['id'], row['concept'], row['entry'], row['mood_id'], row['date'], [])
-                mood = Mood(row['id'], row['mood_label'])
-                entry.mood = mood.__dict__
-                
-            tag = Tag(row['tag_id'],row['tag'])
-            entry.tags.append(tag.__dict__)
-            # does current row id match next row id,
-            # if doesn't, then add entry 
-            try:
-                if dataset[dataset.index(row)]['id'] != dataset[(dataset.index(row)) +1]['id']:              
-                    entries.append(entry.__dict__)
-            except:
-                entries.append(entry.__dict__)
-        return json.dumps(entries)
+#         dataset = db_cursor.fetchall()
+#         for row in dataset:
+#             post = Post(row['id'], row['concept'], row['entry'], row['mood_id'], row['date'], [])
+#             user = User(row['id'], row['mood_label'])
+#             category = Category(row['id'], row['category_label'])
+            
+#             post.user = user.__dict__
+#             post.category = category.__dict__
+            
+#             posts.append = (post.__dict__)
+        
+#         return json.dumps(posts)
