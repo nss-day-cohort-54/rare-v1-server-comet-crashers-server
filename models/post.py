b91ea2e70701188d3b280d55535bbd6f9dfaebcf
Erich Hils <mrerichhils@gmail.com>
2022-04-28 11:25:37 -0500

    feat: commit to update with jacob's changes

--- models/post.py
+++ models/post.py
@@ -1,10 +1,10 @@
 # constructor for post will go here
 class Post():
-    def __init__(self, id, user_id, category_id, title, publication_date, content, image_url=""):
+    def __init__(self, id, user_id, category_id, title, content, publication_date, image_url=""):
         self.id = id
         self.user_id = user_id
-        self.title = title
         self.category_id = category_id
+        self.title = title
+        self.content = content
         self.publication_date = publication_date
         self.image_url = image_url
-        self.content = content
